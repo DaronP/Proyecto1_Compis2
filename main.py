ace1290018f22f0e13f335106d8cbf3784aef39c
DaronP <jandresperez1@yahoo.com>
2022-08-27 10:27:25 -0600

    Praise the frontend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from antlr4 import FileStream, CommonTokenStream
+from antlr4 import FileStream, CommonTokenStream, InputStream
 from ANTLR.Intermediate import Intermediate
 from ANTLR.Visitor import Visitor
 from Console.Console import Error
@@ -7,8 +7,9 @@ from antlr_out.COOLLexer import COOLLexer
 from antlr_out.COOLParser import COOLParser
 
 
-def main(argv: str):
-    input_stream = FileStream(argv)
+def Compile(argv: str):
+    print(argv)
+    input_stream = InputStream(argv)
     lexer = COOLLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = COOLParser(stream)
@@ -32,7 +33,3 @@ def main(argv: str):
         Error(err)
     print()
 
-
-if __name__ == '__main__':
-    main('example.txt')
-    # main(sys.argv)
